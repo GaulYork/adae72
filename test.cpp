6bb7c7f3edcbb73ed0188213e9e897c8e3b4edfb
GaulYork <gaulyork@gmail.com>
2013-04-23 00:53:30 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 int main( void )
 {
   printf("This is just a test 1.\n");
-  printf("Now we are testing add a new commit.\n" );
+  printf("Now we are testing add a new commit, it is changed.\n" );
 
   return 0;
 }
